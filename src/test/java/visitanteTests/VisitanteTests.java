735cb0cc03695da22eabd38f187044d47bbb7f2b
carando <arando.cecilia@gmail.com>
2021-10-28 22:06:07 -0300

    test prueba

--- src/test/java/visitanteTests/VisitanteTests.java
+++ src/test/java/visitanteTests/VisitanteTests.java
@@ -1,5 +1,6 @@
 package visitanteTests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -11,22 +12,25 @@ import visitante.Visitante;
 
 public class VisitanteTests {
 	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
-	
-	
-	/*@Test
-	public void constructor() {
-		Visitante visitante = new Visitante("Luke Skywalker", TipoDeAtraccion.AVENTURA, 100, 8);
-		assertEquals(TipoDeAtraccion.AVENTURA, visitante.getPreferencia());
+	public void constructor_1() {
+		Visitante visitante = new Visitante(1, "Luke Skywalker", "AVENTURA", 100, 8);
+		assertEquals("AVENTURA", visitante.getPreferencia());
 		assertEquals("Luke Skywalker", visitante.getNombre());
 		assertEquals(100, visitante.getPresupuesto(),0.0001);
 		assertEquals(8, visitante.getTiempoDisponibleHs(),0.0001);
 	}
-
+	
+	@Test
+	public void constructor_2() {
+		Visitante visitante = new Visitante(3, "Han Solo", "DEGUSTACION", 36, 8);
+		assertEquals("DEGUSTACION", visitante.getPreferencia());
+		assertEquals("Han Solo", visitante.getNombre());
+		assertEquals(36, visitante.getPresupuesto(),0.0001);
+		assertEquals(8, visitante.getTiempoDisponibleHs(),0.0001);
+	}
+/*
 	@Test
 	public void construccionDeVisitantePriemraLineaDelArchivo() {
 		LecturaDeArchivos manejadorArchivos = new LecturaDeArchivos();
